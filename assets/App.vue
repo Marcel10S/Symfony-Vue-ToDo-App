4d48c5deaf407ce96054fdffa8ebd89432eea852
stulczewskim <marcel.stulczewski@evolpe.pl>
2022-08-26 15:44:56 +0200

    Setup Vue

---
+++ assets/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="container">
+        <h2>Welcome, We have setup Front! :D</h2>
+    </div>
+</template>
+
+<script>
+export default ({
+    data: () => ({
+
+    })
+})
+</script>
+
+<style scoped>
+
+</style>
