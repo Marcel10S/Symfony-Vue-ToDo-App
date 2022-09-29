b52a75dedbbad017a7c945529b7316623fb12922
stulczewskim <marcel.stulczewski@evolpe.pl>
2022-09-29 20:15:48 +0200

    Angular setup

---
+++ task-list/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskBoardComponent } from './task-board/task-board.component';
+import { TaskComponent } from './task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TaskBoardComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
